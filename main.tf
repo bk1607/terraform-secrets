9277228b65dee573fc443db368980fd096b88719
Bala Kishore <baluanumala16@gmail.com>
2024-08-29 20:12:15 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,9 @@
+provider "aws" {
+  region     = var.region
+  access_key = var.access_key
+  secret_key = var.secret_key
+}
+
 resource "aws_s3_bucket" "example" {
   bucket = "my-tf-test-bucket"
 
